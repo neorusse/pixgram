16f77258d12f587ad0e7896a6525054787d38d47
neorusse <neorusse@gmail.com>
2020-05-19 22:16:27 +0100

    Define Users entity

---
+++ pixgram-restapi-user/src/models/Users.ts
@@ -0,0 +1,30 @@
+import { Table, Column, Model, PrimaryKey, CreatedAt, UpdatedAt } from 'sequelize-typescript';
+@Table
+export class Users extends Model<Users> {
+    @Column
+    firstName!: string;
+
+    @Column
+    lastName!: string;
+
+    @PrimaryKey
+    @Column
+    email!: string;
+
+    @Column
+    password!: string;
+
+    @CreatedAt
+    @Column
+    createdAt!: Date;
+
+    @UpdatedAt
+    @Column
+    updatedAt!: Date;
+
+    short(): object {
+        return {
+            email: this.email,
+        };
+    }
+}
