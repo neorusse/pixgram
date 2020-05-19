9e6e6d480ae20cde390e63f86b4799ef361ca55a
neorusse <neorusse@gmail.com>
2020-05-19 23:07:08 +0100

    Setup application

---
+++ pixgram-restapi-feed/src/app.ts
@@ -0,0 +1,44 @@
+/**
+ * Required External Modules
+ */
+
+import * as express from 'express';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import * as bodyParser from 'body-parser';
+import * as errorhandler from 'strong-error-handler';
+import * as logger from 'morgan';
+
+/**
+ * App Variables
+ */
+
+export const app = express();
+
+/**
+ *  App Configuration
+ */
+
+// logs HTTP requests
+app.use(logger('dev'));
+app.use(compression());
+app.use(helmet());
+app.use(cors());
+// middleware for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// middleware for json body parsing
+app.use(bodyParser.json({ limit: '5mb' }));
+
+// Root URI call
+app.get('/', async (req, res) => {
+    res.status(200).json({ info: 'Pixgram RESTFul API Feeds Microservice' });
+});
+
+app.use(
+    errorhandler({
+        debug: process.env.ENV !== 'prod',
+        log: true,
+    }),
+);
