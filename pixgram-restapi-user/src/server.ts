98304d2101f92cab57dbc29fc5abfcd237b301d6
neorusse <neorusse@gmail.com>
2020-05-20 08:43:51 +0100

    Remove drop table if exist rule

--- pixgram-restapi-user/src/server.ts
+++ pixgram-restapi-user/src/server.ts
@@ -5,7 +5,7 @@ import { sequelize } from './sequelize';
 const port = process.env.PORT || 7000;
 
 (async () => {
-    await sequelize.sync({ force: true });
+    await sequelize.sync();
 
     createServer(app).listen(port, () => console.info(`Server running on port ${port}`));
 })();
