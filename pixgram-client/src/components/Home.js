0030b6e0b5da61a242a9636f6cd42a20a2ecc083
neorusse <neorusse@gmail.com>
2020-05-22 21:35:55 +0100

    Create homepage

---
+++ pixgram-client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div className="container">
+			<header className="jumbotron">
+				<h1>Welcome to PixGram</h1>
+			</header>
+		</div>
+	);
+};
+
+export default Home;
