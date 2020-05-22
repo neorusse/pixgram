c6702a15e2e6e6a9f83cb2611202977617d0be03
neorusse <neorusse@gmail.com>
2020-05-22 21:34:54 +0100

    Implemet routing

--- pixgram-client/src/App.js
+++ pixgram-client/src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AuthService from './services/auth.service';
+
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+const App = () => {
+	const [currentUser, setCurrentUser] = useState(undefined);
+
+	useEffect(() => {
+		const user = AuthService.getCurrentUser();
+
+		if (user) {
+			setCurrentUser(user);
+		}
+	}, []);
+
+	const logOut = () => {
+		AuthService.logout();
+	};
+
+	return (
+		<Router>
+			<div>
+				<nav className="navbar navbar-expand navbar-dark bg-dark">
+					<Link to={'/'} className="navbar-brand">
+						PixGram
+					</Link>
+					{currentUser ? (
+						<div className="navbar-nav ml-auto">
+							<li className="nav-item">
+								<Link to={'/profile'} className="nav-link">
+									{`${currentUser.user.firstname.toUpperCase()} ${currentUser.user.lastname.toUpperCase()}`}
+								</Link>
+							</li>
+							<li className="nav-item">
+								<a href="/login" className="nav-link" onClick={logOut}>
+									LogOut
+								</a>
+							</li>
+						</div>
+					) : (
+						<div className="navbar-nav ml-auto">
+							<li className="nav-item">
+								<Link to={'/login'} className="nav-link">
+									Login
+								</Link>
+							</li>
+
+							<li className="nav-item">
+								<Link to={'/register'} className="nav-link">
+									Sign Up
+								</Link>
+							</li>
+						</div>
+					)}
+				</nav>
+
+				<div className="container mt-3">
+					<Switch>
+						<Route exact path={['/']} component={Home} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/profile" component={Profile} />
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
